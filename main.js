a0c38b4850d8749c4beb849c6da38ace0ef000b0
AanyaKumar <74505825+AanyaKumar@users.noreply.github.com>
2020-11-18 09:46:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My first console project");
+
+var first_name = "Aanya";
+var last_name = "Kumaru";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 23;
+var b = 8;
+
+var add = a + b ;
+console.log(add);
+
+var sub = a - b ;
+console.log(sub);
+
+var multiple = a * b ;
+console.log(multiple);
+
+var divide = a / b ;
+console.log(divide);
